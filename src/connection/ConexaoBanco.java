bf4d505536d1b57a158037a4e4b47108ade2d061
andradevictor1 <victorandrade1994@gmail.com>
2018-07-05 00:50:22 -0300

    agora vai

--- src/connection/ConexaoBanco.java
+++ src/connection/ConexaoBanco.java
@@ -5,10 +5,45 @@
  */
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Victor
  */
 public class ConexaoBanco {
     
+    
+    
+    private String servidor;
+    private String banco;
+    private String usuario;
+    private String senha;
+    private Connection conexao;
+    
+    public ConexaoBanco()
+    {
+        this.servidor = "localhost";
+        this.banco = "RegistroMerenda";
+        this.usuario = "root";
+        this.senha = "root";
+    }
+    
+    public boolean conectar(){
+        try
+        {
+            this.conexao = DriverManager.getConnection("jdbc:mysql://"+this.servidor+"/"+this.banco,this.usuario,this.senha);
+            return true;
+        }
+        catch(SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public Connection getConnection() {
+        return conexao;
+    }
+
 }
--- src/dao/AlunoDao.java
+++ src/dao/AlunoDao.java
@@ -5,10 +5,50 @@
  */
 package dao;
 
+import connection.ConexaoBanco;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelos.Aluno;
+
 /**
  *
  * @author Victor
  */
 public class AlunoDao {
+  
+    private ConexaoBanco conexao;
+    
+    public AlunoDao()
+    {
+        this.conexao = new ConexaoBanco();
+    }
+    
+    
+    public void inserir(Aluno aluno) {
+        //string com a consulta que será executada no banco
+        String sql = "INSERT INTO alunos (id,nome) VALUES (?,?)";
+        
+        try
+        {
+            //tenta realizar a conexão, se retornar verdadeiro entra no IF
+            if(this.conexao.conectar())
+            {
+                //prepara a sentença com a consulta da string
+                PreparedStatement sentenca = this.conexao.getConnection().prepareStatement(sql);
+                
+                //subtitui as interrograções da consulta, pelo valor específico
+                sentenca.setString(1,aluno.getNome()); //subsitui a primeira ocorrência da interrogação pelo atributo nome
+                
+                
+                sentenca.execute(); //executa o comando no banco
+                sentenca.close(); //fecha a sentença
+                this.conexao.getConnection().close(); //fecha a conexão com o banco
+            }
+        }
+        catch(SQLException ex)
+        {
+           throw new RuntimeException(ex);
+        }
+    }
     
 }
