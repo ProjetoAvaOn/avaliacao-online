90e8e39252c7df4e53d82d72da32ab90348b83fa
andradevictor1 <victorandrade1994@gmail.com>
2018-07-05 01:16:06 -0300

    05/07

---
+++ test/modelos/ClasseDeTesteGitTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Victor
+ */
+public class ClasseDeTesteGitTest {
+    
+    public ClasseDeTesteGitTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
---
+++ test/modelos/ModelosSuite.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Victor
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({modelos.ClasseDeTesteGitTest.class, modelos.OpQuestaoTest.class, modelos.ProfessorTest.class, modelos.QuestaoTest.class, modelos.NewClassTest.class, modelos.MateriaTest.class, modelos.AlunoTest.class, modelos.CursoTest.class})
+public class ModelosSuite {
+    
+}
