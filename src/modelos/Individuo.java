a3edd1600c3e41352f0444da60870986fb021672
andradevictor1 <victorandrade1994@gmail.com>
2018-07-09 18:33:36 -0300

    classe banco

--- src/modelos/Individuo.java
+++ src/modelos/Individuo.java
@@ -10,9 +10,30 @@ package modelos;
  * @author Victor
  */
 public class Individuo {
+
+    public Individuo(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
     
     
     private String nome;
     private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
